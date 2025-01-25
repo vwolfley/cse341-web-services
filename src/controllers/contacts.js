31a7586bf3a7a1f5431a022ccf5c8c6c9068e5dc
vern wolfley <vern.wolfley@gmail.com>
2025-01-24 20:26:39 -0700

    updates for week 3

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -10,33 +10,108 @@ const contactsController = {};
 
 // Data for html page
 contactsController.getData = async function (req, res, next) {
+  try {
     const result = await mongodb.getDb().db().collection("contacts").find();
     result.toArray().then((lists) => {
-        res.setHeader("Content-Type", "application/json");
-        res.status(200).json(lists);
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists);
     });
+  } catch (error) {
+    res.status(500).json({ message: "An unexpected error occurred.", error: error.message });
+  }
 };
 
+// Get all contacts
 contactsController.getAll = async (req, res, next) => {
+  try {
     const result = await mongodb.getDb().db().collection("contacts").find();
     result.toArray().then((lists) => {
-        res.setHeader("Content-Type", "application/json");
-        res.status(200).json(lists);
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists);
     });
+  } catch (error) {
+    res.status(500).json({ message: "An unexpected error occurred.", error: error.message });
+  }
 };
 
+// Get contact by id
 contactsController.getById = async (req, res, next) => {
-    // const userId = new ObjectId(req.params.id);
+  try {
     const userId = ObjectId.createFromHexString(req.params.id);
-    const result = await mongodb
-        .getDb()
-        .db()
-        .collection("contacts")
-        .find({ _id: userId });
+    const result = await mongodb.getDb().db().collection("contacts").find({ _id: userId });
     result.toArray().then((lists) => {
-        res.setHeader("Content-Type", "application/json");
-        res.status(200).json(lists[0]);
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists[0]);
     });
+  } catch (error) {
+    res.status(500).json({ message: "An unexpected error occurred.", error: error.message });
+  }
+};
+
+// Post - Insert contact into the database
+contactsController.createContact = async (req, res, next) => {
+  try {
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday,
+    };
+
+    const response = await mongodb.getDb().db().collection("contacts").insertOne(contact);
+    if (response.acknowledged) {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201);
+      res.json({ message: "Contact created successfully.", contactId: response.insertedId });
+    } else {
+      res.status(500).json({ message: "Failed to create contact." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "An unexpected error occurred.", error: error.message });
+  }
+};
+
+// Put - update contact in the database
+contactsController.updateContactById = async (req, res, next) => {
+  try {
+    const userId = ObjectId.createFromHexString(req.params.id);
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday,
+    };
+
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection("contacts")
+      .replaceOne({ _id: userId }, contact);
+    if (response.modifiedCount > 0) {
+      res.status(204);
+    } else {
+      res.status(404).json({ message: "Failed to update contact.", error: response.error });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "An unexpected error occurred.", error: error.message });
+  }
+};
+
+// Delete - delete contact from the database
+contactsController.deleteContactById = async (req, res, next) => {
+  try {
+    const userId = ObjectId.createFromHexString(req.params.id);
+    const response = await mongodb.getDb().db().collection("contacts").deleteOne({ _id: userId });
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: "Contact not found.", error: response.error });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "An unexpected error occurred.", error: error.message });
+  }
 };
 
 module.exports = contactsController;
